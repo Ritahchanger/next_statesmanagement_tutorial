5f728acb53866a39e5fe38cacafb1326691fc6df
Dennis Peter <peterdennis573@gmail.com>
2025-03-30 22:38:39 +0300

    Updates

--- app/auth/page.tsx
+++ app/auth/page.tsx
@@ -1,8 +1,62 @@
+"use client"
 import React from 'react'
 
+import { useSelector,useDispatch } from 'react-redux'
+
+import { RootState,AppDispatch } from '@/store/Store'
+
+import { login,logout } from '@/store/features/AuthenticationSlice'
+
 const page = () => {
+
+  const dispatch = useDispatch<AppDispatch>();
+
+  const { user,isAuthenticated  } = useSelector((state:RootState)=>state.auth);
+
+  const handleLogin = () =>{
+
+    dispatch(
+
+        login({
+
+            id:"123",
+
+            name:"John Doe",
+
+            email:"john@example.com",
+
+            token:"fakse-jwt-token",
+        
+        })
+
+    )
+  }
+
+
+  const handleLogout = () =>{
+
+    dispatch(logout());
+
+  }
+
   return (
-    <div>page</div>
+    <div className='w-full h-[100vh] text-center flex justify-center items-center'>
+
+        { isAuthenticated ? (
+
+            <div>
+
+                <p>Welcome, {user?.name}</p>
+
+                <button onClick={handleLogout}>LOGOUT</button>
+
+            </div>
+
+        ) : (
+            <button onClick={handleLogin}>LOGIN</button>
+        ) }
+
+    </div>
   )
 }
 
